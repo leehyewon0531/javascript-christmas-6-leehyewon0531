301495e25275ea7c2218719b263f365fc7e7135d
leehyewon0531 <ifpffk@naver.com>
2023-11-14 15:17:24 +0900

    feat: 사용자 입력 받는 부분 작성

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import OutputView from "./OutputView.js";
 class App {
   async run() {
     OutputView.printWelcomeMsg();
-    InputView.readDate();
+    const visitDate = await InputView.readDate();
+    // visitDate 유효성 검사
+
+    const menuAndQuantity = await InputView.readMenu();
+    // menuAndQuantity 유효성 검사
+
+    OutputView.printEventBenefitMsg();
   }
 }
 
