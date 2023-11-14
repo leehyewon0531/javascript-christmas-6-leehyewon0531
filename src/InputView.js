e7cb330f3671ac9eab972e9675306b7ebcb5e831
leehyewon0531 <ifpffk@naver.com>
2023-11-14 13:38:09 +0900

    feat: 예상 방문 날짜 입력 받는 함수 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,9 +1,10 @@
+import { GUIDE_MSG } from "./constants/guideMsg";
+
 const InputView = {
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+        const input = await Console.readLineAsync(GUIDE_MSG.visitDate);
+        return input;
     }
-    // ...
 }
 
 export default InputView;
