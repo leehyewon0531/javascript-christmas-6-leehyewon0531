9726a4c2b5dc93775d3c2b5314d77271abfecc93
leehyewon0531 <ifpffk@naver.com>
2023-11-14 12:19:25 +0900

    feat: 가이드 메시지 상수 정의

---
+++ src/constants/guideMsg.js
@@ -0,0 +1,16 @@
+export const GUIDE_MSG = {
+  welcome: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+  visitDate: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  menuAndQuantity: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+  eventBenefit: '우테코 식당에서 받을 이벤트 혜택 미리 보기!',
+}
+
+export const BENEFIT_LIST = {
+  orderMenu: '<주문 메뉴>',
+  beforeDiscount: '<할인 전 총주문 금액>',
+  giveaway: '<증정 메뉴>',
+  benefitDetails: '<혜택 내역>',
+  totalBenefit: '<총혜택 금액>',
+  afterDiscount: '<할인 후 예상 결제 금액>',
+  eventBadge: '<12월 이벤트 배지>'
+}
