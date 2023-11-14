25185243a3791ad763741e2774a76aa737b7438d
leehyewon0531 <ifpffk@naver.com>
2023-11-14 14:50:04 +0900

    chore: InputView tab size 조정

--- src/InputView.js
+++ src/InputView.js
@@ -1,15 +1,15 @@
 import { GUIDE_MSG } from "./constants/guideMsg";
 
 const InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync(GUIDE_MSG.visitDate);
-        return input;
-    },
+	async readDate() {
+		const input = await Console.readLineAsync(GUIDE_MSG.visitDate);
+		return input;
+	},
 
-    async readMenu() {
-        const input = await Console.readLineAsync(GUIDE_MSG.menuAndQuantity);
-        return input;
-    }
+	async readMenu() {
+		const input = await Console.readLineAsync(GUIDE_MSG.menuAndQuantity);
+		return input;
+	}
 }
 
 export default InputView;
