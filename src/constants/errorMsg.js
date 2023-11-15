b3c42cbbc397372f0ecad3929e7e3865de62add7
leehyewon0531 <ifpffk@naver.com>
2023-11-15 15:27:18 +0900

    feat: 오류 메시지 상수 추가

---
+++ src/constants/errorMsg.js
@@ -0,0 +1,9 @@
+export const ERROR_MSG = {
+  invalidDate: '식당 방문 날짜는 1 이상 31 이하의 숫자여야 합니다.',
+  notInMenu: '메뉴판에 있는 메뉴만 구매 가능합니다.',
+  invalidMenuAmount: '메뉴 개수는 1 이상의 숫자여야 합니다.',
+  invalidMenuFormat: '메뉴는 예시 형식에 맞게 입력해야 합니다.',
+  duplicateMenu: '메뉴는 중복되지 않게 입력해야 합니다.',
+  overTwenty: '한 번에 20개 이상의 메뉴는 구매할 수 없습니다.',
+  onlyBeverage: '음료만 주문할 수 없습니다.'
+}
